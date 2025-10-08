c09e091d4eeda76a1a0065ea6d1c05072e8e0083
Chaante-lee <ST10481788@vcconnect.edu.za>
2025-10-08 17:26:04 +0200

    new commit

---
+++ script.js
@@ -0,0 +1,8 @@
+var firstName = prompt("what is your first name");
+var lastName = prompt("what is your last name");
+var age = prompt("what is your age ") 
+
+var fulName = firstName + "" + lastName
+
+window.alert("your full name is " + fulName);
+window.alert("you are " + age + " years old")
